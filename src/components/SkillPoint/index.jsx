857049a491a2203f597949c32730bf8a32408c2a
Pedro Werkhaizer <42971669+pedrowerkhaizer@users.noreply.github.com>
2022-02-05 13:49:08 -0300

    Updated "About me" page

--- src/components/SkillPoint/index.jsx
+++ src/components/SkillPoint/index.jsx
@@ -1,18 +1,19 @@
+'esversion: 6;'
 import React from 'react';
 import '../../App.css';
 
 //lista {
-    // 1: {title: 'X', type: 'skill', 'expertise: 0.8'}
-    // 2: {title: 'Y', type: 'skill', 'expertise: 0.97'}
-    // foreach item lista { <div display vertical> <div display vertical> <p>{item.title}</p> </div> </div>}
+// 1: {title: 'X', type: 'skill', 'expertise: 0.8'}
+// 2: {title: 'Y', type: 'skill', 'expertise: 0.97'}
+// foreach item lista { <div display vertical> <div display vertical> <p>{item.title}</p> </div> </div>}
 
-const SkillPoint = (type, expertise) => {
-    let color = "#000"
-    //If type is equal to "Skill" then show yellow, if not, show blue
-    type = 'skill' ? color= '#F4F681' : color = '#73C1ED'
+const SkillPoint = (props) => {
     return (
-        <div style={{width: '28vw', height: '0.8rem', borderRadius: '0.5rem', border: '2 solid #353A44'}}>
-            <div style={{height: '100%', borderRadius: '1rem', backgroundColor: {color}, width: {expertise}}} />
+        <div>
+            <p style={{ fontSize: 16, margin: '0.5rem 0', fontWeight: 500, color: '#DDDDDD' }}>{props.title}</p>
+            <div style={{ maxWidth: '20vw', minWidth: 256, height: 16, borderRadius: '0.5rem', border: '2px solid #353A44' }}>
+                <div style={{ height: '100%', borderRadius: '1rem', backgroundColor: props.color, width: props.expertise }} />
+            </div>
         </div>
     )
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,33 @@ import React from 'react';
 import '../App.css'
 
 import photo from '../assets/me_yellowbg.png'
+import SkillPoint from '../components/SkillPoint';
 
 class About extends React.Component {
     render() {
+        const skillList = [
+            { title: 'JavaScript', expertise: "95%" },
+            { title: 'ReactJS', expertise: "88%" },
+            { title: 'ReactNative', expertise: "85%" },
+            { title: 'Java', expertise: "82%" },
+            { title: 'Python', expertise: "80%" },
+            { title: 'Photoshop', expertise: "80%" },
+            { title: 'Figma', expertise: "80%" },
+            { title: 'HTML 5', expertise: "95%" },
+            { title: 'MySQL', expertise: "80%" },
+            { title: 'noSQL', expertise: "80%" },
+            { title: 'Node.js', expertise: "75%" },
+            { title: 'C#', expertise: "85%" },
+            { title: 'Illustrator', expertise: "70%" },
+            { title: 'PHP', expertise: "70%" },
+            { title: 'Agile Scrum', expertise: "78%" },
+        ]
+
+        const languagesList = [
+            { title: 'Portuguese', expertise: "100%" },
+            { title: 'English', expertise: "88%" },
+            { title: 'Spanish', expertise: "65%" },
+        ]
         return (
             <div className="contentContainer" style={{ alignItems: 'start', marginTop: '3rem', display: 'flex', flexDirection: 'column' }}>
                 <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
@@ -22,8 +46,32 @@ class About extends React.Component {
                     When I was a child, every time I saw a game I wondered how it worked. This curiosity moved me and at the age of 14, 2 years after some freelance experiences in Graphic Design, I decided to create my first mobile game using C# and JavaScript technology through the Unity Engine and ended up falling in love with this world. (Although I realized I didn't come to this world to create games lol)
                 </p>
 
-                <h6 style={{ letterSpacing: 2, margin: '1.5rem 0 1rem 0', color: '#9BA4CD' }}>ABOUT ME</h6>
-                
+                <h6 style={{ letterSpacing: 2, margin: '1.5rem 0 0.5rem 0', color: '#9BA4CD' }}>SKILLS</h6>
+                <div className="skillContainer">
+                    {skillList.map(({ title, expertise }, index) => (
+                        <SkillPoint
+                            key={index}
+                            color="#e0af1c"
+                            title={title}
+                            expertise={expertise} />
+                    ))}
+                </div>
+
+                <h6 style={{ letterSpacing: 2, margin: '1.5rem 0 0.5rem 0', color: '#9BA4CD' }}>LANGUAGES</h6>
+                <div className="skillContainer">
+                    {languagesList.map(({ title, expertise }, index) => (
+                        <SkillPoint
+                            key={index}
+                            color="#3BACEC"
+                            title={title}
+                            expertise={expertise} />
+                    ))}
+                </div>
+
+                <div className="historicalContainer">
+                    <h6 style={{ letterSpacing: 2, margin: '1.5rem 0 0.5rem 0', color: '#9BA4CD', paddingBottom: '4rem' }}>ACADEMIC HISTORIC</h6>
+                </div>
+
             </div>
         )
     }
